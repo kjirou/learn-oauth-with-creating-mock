f6ce19ff03ec87eb7fe0e4c0f0c4d91ef24f9bb6
kjirou <sorenariblog@gmail.com>
2022-01-24 10:40:11 +0900

    Add servers

---
+++ authorization-server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const portArg = process.argv[2]
+if (portArg === undefined) {
+  throw new Error('Specify the port for the authorization server as the first argument.')
+}
+const port = parseInt(portArg)
+
+const app = express()
+
+app.get('/', function (req, res) {
+  const html = `<html><body>
+    <h1>Authorization Server</h1>
+  </body></html>`
+  res.set({
+    'Content-Type': 'text/html',
+    'Cache-Control': 'no-store',
+  })
+  res.send(html)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on ${port} port.`)
+})
